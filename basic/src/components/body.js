a62ee492824cae48d4f0a6b595bb286f129141e5
Forrest Bingham <bingham_5@live.com>
2020-09-08 14:32:01 -0600

    Finished Upper Body

--- basic/src/components/body.js
+++ basic/src/components/body.js
@@ -1,10 +1,29 @@
 import React from "react";
+import "./body.css";
 
 const Body = () => {
 
     return (
         <div className="body">
-            <h1>Body!</h1>
+            <div className="upper">
+                <div className="backgroundImage"/>
+                <div className="bodyIntro">
+                    <h1 className="upperTitle">Welcome to our homepage!</h1>
+                <div className="upperAbout">
+                    <p className="upperText">This is who we are and some other neat things about us.
+                    We aim to achieve this goal by delivering some cool products to better your life.
+                    We are a company located in a city near you.
+                    Please tell all of your friends about us and maybe we can give you some free stuff idk though.</p>  
+                </div>
+                    <button className="upperButton">Learn More</button>
+                </div>
+            </div>
+            <div className="middle">
+            <h2>Middle Body!</h2>
+            </div>
+            <div className="lower">
+            <h1>Lower Body</h1>
+            </div>
         </div>
     )
 }
