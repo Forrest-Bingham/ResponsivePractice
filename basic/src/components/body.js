d3fa12fd3df7b9acc09a25fd79ee565426bf2e23
Forrest Bingham <bingham_5@live.com>
2020-09-10 22:34:11 -0600

    Finished layouts

--- basic/src/components/body.js
+++ basic/src/components/body.js
@@ -1,5 +1,8 @@
 import React from "react";
 import "./body.css";
+import Goomby from "../images/Goomby.jpg";
+import SirReginald from "../images/SirReginald.jpg";
+import LordF from "../images/LordF.jpg";
 
 const Body = () => {
 
@@ -19,10 +22,38 @@ const Body = () => {
                 </div>
             </div>
             <div className="middle">
-            <h2>Middle Body!</h2>
+                <div className="middleInfo">
+                    <img className="personImage" src={Goomby} alt="Goomby"/>
+                    <h3>Goomby - Social Media Manager</h3>
+                    <p>We don't know where he is from but he does a great job! <br/>
+                    Has about 3 months of experience and learned all he knows <br/>
+                    from watching videos on Youtube</p>
+                </div>
+                <div className="middleInfo">
+                    <img className="personImage" src={SirReginald} alt="Sir Reginald"/>
+                    <h3>Sir Reginald - Chief Financial Officer</h3>
+                    <p>Sir Reginald is the Duke of Chutney. Therefore he is of <br/>
+                    royal decent and has lots of money! He left his kingdom to come work for us. Wow what a great guy</p>
+                </div>
+                <div className="middleInfo">
+                    <img className="personImage" src={LordF} alt="Lord Farquaad"/>
+                    <h3>Lord Farquaad - CEO</h3>
+                    <p>He is the CEO of our company because he has all the money <br/> 
+                    and we all work in his castle. It's not easy working for him <br/>
+                    because some of us may die, but that is a sacrifice he is willing to make.</p>
+                </div>
             </div>
             <div className="lower">
-            <h1>Lower Body</h1>
+                <div className="lowerLeft">
+                    <div className="lowerLeftFirst">
+                        <h1>Title!</h1>
+                    </div>
+                    <div className="lowerLeftSecond">
+                        <h2>Title 2!</h2>
+                    </div>
+                </div>
+                <div className="lowerRight"/>
+                
             </div>
         </div>
     )
